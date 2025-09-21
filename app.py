9a3ea0898aedcdf96778b77577fa509ec37d5fe4
Aymane-kr <aymameraf2007@gmail.com>
2025-09-21 15:08:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ print(f"the location of this number is: {geocoder.description_for_number(phone_n
 
 print(f"the carrier of this number is : {carrier.name_for_number(phone_num, "en")}")
 
-print(f"the timezone of this number is:{timezone.time_zones_for_geographical_number(phone_num)}")
+
+print(f"the timezone of this number is:{timezone.time_zones_for_number(phone_num)}")
